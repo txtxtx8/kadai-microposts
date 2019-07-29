471b8c439f97c99bc35cf82995b7933aa8e80771
y.to <suteakaga117@gmail.com>
2019-07-29 16:43:20 +0000

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.order(id: :desc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザを登録しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗'
+      render :new
+    end
+  end
+end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
+  resources :users , only: [:new,:index,:show,:create]
+  get 'signup',to: 'users#new'
 end
