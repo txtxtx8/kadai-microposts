a65ff320ab1b6b58b05c9f9ea9d61c7c5c3982d1
y.to <suteakaga117@gmail.com>
2019-08-07 19:49:18 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,15 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :users , only: [:new,:index,:show,:create]
   get 'signup',to: 'users#new'
+  resources :users , only: [:new,:index,:show,:create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  
   
   resources :microposts , only: [:create , :destroy]
+  resources :relationships , only: [:create , :destroy]
 end
