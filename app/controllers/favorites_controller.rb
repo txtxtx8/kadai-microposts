7517d491afaf19666d5085771586670c628c3a2e
y.to <suteakaga117@gmail.com>
2019-08-10 13:34:51 +0000

    re

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,14 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.like(micropost)
     flash[:success] = '投稿をお気に入りに追加しました。'
-    redirect_to current_user
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unlike(micropost)
     flash[:success] = '投稿をお気に入りから削除しました'
-    redirect_to current_user
+    redirect_back(fallback_location: root_path)
   end
 
 end
